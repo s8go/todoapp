e012fa3f262a67f795d2d87835a797586c521c9a
Segun Owolabi <78587948+segzle@users.noreply.github.com>
2022-02-18 05:31:59 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+let addbtn = document.querySelector(".add");
+let input = document.querySelector("#inp");
+let todo = document.querySelector("#todo");
+let label = document.querySelector("#lab");
+
+let name = prompt("Please, input your name");
+
+label.innerHTML = `What are you doing today? ${name}`;
+
+addbtn.textContent = "Add";
+
+//Adding function to the Add button
+
+addbtn.addEventListener("click", () => {
+  let newval = input.value;
+  if (newval) {
+    todo.innerHTML += `<li id="todolist"><p>${newval}</p>
+    <span>
+    <p class="edit">Edit</p>
+    <p class="delete">Delete</p>
+    </span></li>`;
+    input.value = "";
+  }
+
+  //Adding function to the Delete button
+
+  let del = document.querySelectorAll(".delete");
+
+  del.forEach((dl) => {
+    dl.addEventListener("click", delFun);
+    function delFun() {
+      let dd = this.parentNode;
+      dd.parentNode.remove();
+    }
+  });
+
+  let edit = document.querySelectorAll(".edit");
+
+  //Adding function to the Edit button
+
+  edit.forEach((ed) => {
+    ed.addEventListener("click", edFun);
+    function edFun() {
+      let span = this.parentNode;
+      let li = span.parentNode;
+      let p = li.querySelector("p");
+
+      let inpt = document.createElement("input");
+      inpt.value = p.innerHTML;
+      li.replaceChild(inpt, p);
+
+      let save = document.createElement("p");
+      save.classList.add("save");
+      save.innerHTML = "Save";
+      span.replaceChild(save, this);
+
+      //Adding function to the save button
+
+      save.addEventListener("click", () => {
+        p.innerHTML = inpt.value;
+        li.replaceChild(p, inpt);
+        let edd = document.createElement("p");
+        edd.classList.add("edit");
+        edd.innerHTML = "Edit";
+        span.replaceChild(edd, save);
+        edd.addEventListener("click", edFun);
+      });
+    }
+  });
+});
